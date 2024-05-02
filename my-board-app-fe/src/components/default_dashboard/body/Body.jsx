81619232901d978c038a6e3e3937110e0af69400
evaloper <gbajeeva@yahoo.com>
2024-05-02 18:42:25 +0100

    Populate dashboard

--- my-board-app-fe/src/components/default_dashboard/body/Body.jsx
+++ my-board-app-fe/src/components/default_dashboard/body/Body.jsx
@@ -1,21 +1,59 @@
-import { Link } from 'react-router-dom';
-import image1 from '../../../assets/images/add1.png';
-import image from '../../../assets/images/img2.png';
+import React, { useState } from "react";
+import image1 from "../../../assets/images/add1.png";
+import image2 from "../../../assets/images/three-dots.png";
+import EditMenu from "../../populated_dashboard/edit_menu/EditMenu";
+import image from "../../../assets/images/img2.png";
 
-const Body = ({hideModal}) => {
- 
-
-  return(
-  <div className='pt-[13rem] w-[1215px]'>
-    <div className='flex justify-center items-center '>
-      <img className=' bg-[50%_50%] bg-cover bg-no-repeat w-[27.9rem] h-[25.8rem] mx-auto ' src={image} alt="" />
-    </div>
-    <p className='text-center font-lato font-bold text-2xl leading-7 text-[#475467] mt-6'>A fresh list, let's get started</p>
-    <div className='flex items-end flex-col '>
-    <button onClick={hideModal}  className=' mr-[6.4rem]'><img src={image1} alt="" /></button>
-       <p className=' mr-[5rem] mt-1 text-10px leading-11px text-#101828'>Create a task</p>
-    </div>
-  </div>
-)};
+const Body = ({ hideModal, tasks }) => {
+  const [showEditMenu, setShowEditMenu] = useState(false);
+  const ToggleEditMenuModal = () => {
+    setShowEditMenu((showEditMenu) => !showEditMenu);
+  };
+  return (
+    <>
+      {showEditMenu && <EditMenu toggleEditMenuModal={ToggleEditMenuModal} />}
+      <div className="pt-[13rem] w-[1215px]">
+        {!tasks && (
+          <>
+            <div className="flex justify-center items-center ">
+              <img
+                className=" bg-[50%_50%] bg-cover bg-no-repeat w-[27.9rem] h-[25.8rem] mx-auto "
+                src={image}
+                alt=""
+              />
+            </div>
+            <p className="text-center font-lato font-bold text-2xl leading-7 text-[#475467] mt-6">
+              A fresh list, let's get started
+            </p>
+          </>
+        )}
+        {tasks &&
+        tasks.map((task) => (
+          <div key={task.id} className="flex items-center justify-between border-b-[#EAECF0] border-t-0 border-r-0 border-l-0 border-2 p-5">
+            <div className="flex items-center justify-between ">
+              <input
+                type="radio"
+                name="Go to the movies by 7pm with Nonso"
+                id=""
+              />
+              <p className="pl-5 text-[#667085]">{task.title} </p>
+            </div>
+            <button onClick={ToggleEditMenuModal}>
+              <img src={image2} alt="" className="mr-[68px]" />
+            </button>
+          </div>
+        ))}
+        <div className="flex items-end flex-col ">
+          <button onClick={hideModal} className=" mr-[6.4rem]">
+            <img src={image1} alt="" />
+          </button>
+          <p className=" mr-[5rem] mt-1 text-10px leading-11px text-#101828">
+            Create a task
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Body;
--- my-board-app-fe/src/components/default_dashboard/header/Header.jsx
+++ my-board-app-fe/src/components/default_dashboard/header/Header.jsx
@@ -15,8 +15,8 @@ const Header = ({selectedListName, toggleMenuModal}) => {
       <nav className=' absolute z-0 w-[85%] ml-[225] bg-[#175CD3] h-[134px] '>
         <div className='flex justify-between h-8 pr-[4rem]  pt-6'>
           <div className='flex '>
-            <img src={List} alt="" className='w-[2rem] h-[1.7rem]'/>
-            <Link to='/dashboard' className=" text-white font-lato  text-xl leading-tight pl-2">{selectedListName}</Link>
+            <div src={List} alt="" className='w-[2rem] h-[1.7rem]'/>
+            <Link to='/dashboard' className=" text-white font-lato  text-xl leading-tight pl-2"></Link>
           </div>
           <div className='flex  gap-3 mr-[2rem]'>
             <h4 className=' mt-1.5 text-white font-lato text-xl  leading-tight'>Hey, {userName}</h4>
@@ -24,7 +24,7 @@ const Header = ({selectedListName, toggleMenuModal}) => {
           </div>
         </div>
         <div className='flex justify-between mt-12 pr-[4rem] '>
-          <img className='w-[50px] h-[50px]' src={sublist} alt="" />
+          <div className='w-[50px] h-[50px]' src={sublist} alt="" />
           <div className='flex gap-2 h-8 mr-[2rem] '>
             <img src={add} alt="" className='w-8'/>
             <button  onClick={toggleMenuModal}><img src={Dot} alt="" /></button>
