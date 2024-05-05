f25245731c4cc381c52b6a023583cc2d2ba17d7a
evaloper <gbajeeva@yahoo.com>
2024-05-05 17:48:47 +0100

    Populated Dashboard

--- my-board-app-fe/src/api-service/index.js
+++ my-board-app-fe/src/api-service/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYm5lcnZpdmlhbkBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6IlVTRVIiLCJpYXQiOjE3MTQ0MTY4MjEsImV4cCI6MTcxNDUwMzIyMX0._Rw7T9_v9TWsKbCjkUkPZJ2qa6h_LyJbiu-FpfWubeQ";
+const token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvZ2hlbmVmZWppcm9nYmFqZUBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6IlVTRVIiLCJpYXQiOjE3MTQ2NzcxMzIsImV4cCI6MTcxNDc2MzUzMn0.J-fjadJcfbIZosTdAFBxKBNjlaPhs1yZtEvUK_m_aAn4G4lu3wPu7fOY2N_k7ZKaVK0PnQI44XRQ_lvMv2ZuQw";
 const REACT_APP_BACKEND_HOST= "http://localhost:8080/api/v1";
 console.log('backend host is ', REACT_APP_BACKEND_HOST);
 const ApiQuery = axios.create({
