ec4f5523c63c4ea4731c261a46a119e1b4e3742e
evaloper <gbajeeva@yahoo.com>
2024-05-06 21:49:35 +0100

    Remove comment

--- my-board-app-fe/src/components/ProtectedRoute.jsx
+++ my-board-app-fe/src/components/ProtectedRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export const ProtectedRoute = () => {
   const isAuthenticated = localStorage.getItem("token");
-  // console.log(isAuthenticated);
 
   if (!isAuthenticated) {
     console.log("back to login");
