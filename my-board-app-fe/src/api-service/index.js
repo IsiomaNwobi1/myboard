a800cc9e3f9016e17a844e1437a64fa37d752102
Vivian Abner <abnervivian@gmail.com>
2024-05-07 00:01:05 +0100

    integration

--- my-board-app-fe/src/api-service/index.js
+++ my-board-app-fe/src/api-service/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYm5lcnZpdmlhbkBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6IlVTRVIiLCJpYXQiOjE3MTQ0MTY4MjEsImV4cCI6MTcxNDUwMzIyMX0._Rw7T9_v9TWsKbCjkUkPZJ2qa6h_LyJbiu-FpfWubeQ";
-const REACT_APP_BACKEND_HOST= "http://localhost:8080/api/v1";
+const token ="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhYm5lcnZpdmlhbkBnbWFpbC5jb20iLCJhdXRob3JpdGllcyI6IlVTRVIiLCJpYXQiOjE3MTQ2NzgwMDksImV4cCI6MTcxNDc2NDQwOX0.z956tV4JM6cBW7XnYLN_i_FAG3qvvXUIXN6aKI2_slk";
+const REACT_APP_BACKEND_HOST= "http://localhost:8081/api/v1";
 console.log('backend host is ', REACT_APP_BACKEND_HOST);
 const ApiQuery = axios.create({
     baseURL: REACT_APP_BACKEND_HOST,
